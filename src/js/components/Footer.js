8b2401099c74fc55990e0eb701fef7265d20f855
Hunter <jenningsdhunter@gmail.com>
2019-08-04 21:39:37 -0400

    Adding GA and email link

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -6,18 +6,19 @@ export default function Footer() {
       <div className="footer--wrapper">
         <div className="col">
           <p>
-            This website was built with React. Framer Motion for animation. All projects are available on Github.
+            This website was built with React. Framer Motion for animation.
+            All projects are available on Github.
           </p>
         </div>
         <div className="col">
           <a href="https://github.com/h-jennings" target="_blank" rel="noopener noreferrer">
             Github.
           </a>
-          <a href="https://www.instagram.com/h_jennings/" target="_blank" rel="noopener noreferrer">
-            Instagram.
-          </a>
           <a href="https://twitter.com/jennings_hunter" target="_blank" rel="noopener noreferrer">
             Twitter.
+          </a>
+          <a href="mailto:jenningsdhunter@gmail.com">
+            Email.
           </a>
         </div>
       </div>
